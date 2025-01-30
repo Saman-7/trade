4481286ee36ae8008f430f9f5ef2f85a288ef8cc
Saman-7 <Saman7Dev@gmail.com>
2025-01-30 14:53:22 +0330

    Adds save crypto candles for specified timeframes

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,25 @@
-import { Symbols } from "./constants";
-import { getDetail } from "./services";
+import { Symbols, Timeframe } from "./constants";
+import { getCandles } from "./services";
+import { doesFileExist, saveCandlesToFile } from "./utils";
 
-console.log("Bot Starting ...");
+console.log("🤖 Bot Starting ...");
 
-const details = await getDetail(Symbols.BTC);
-console.log(details);
+////////////////////////////////////////////////////////////////////////////////////////
 
-console.log("Bot Ending ...");
+// Save Candles for Timeframes
+const saveTimeframes: Array<Timeframe> = ["1m", "3m", "5m"];
+for (const timeframe of saveTimeframes) {
+  const customDir = "data";
+  const exists = await doesFileExist(customDir, `${timeframe}.json`);
+  if (!exists) {
+    console.log(`⚙️  No Candles Found for ${timeframe}, Creating ...`);
+    const candles = await getCandles(Symbols.BTC, timeframe, "days", 1);
+    await saveCandlesToFile(timeframe, candles);
+  } else {
+    console.log(`✅ Candles Found for ${timeframe}, Skipping ...`);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+console.log("🤖 Bot Ending ...");
