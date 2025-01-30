a463560ea8a00059366ed1bded90b9b42e99eb73
Saman-7 <Saman7Dev@gmail.com>
2025-01-30 16:31:59 +0330

    Adds divergence calculation functionality to indicators

---
+++ src/indicators/divergence.ts
@@ -0,0 +1,19 @@
+type Divergence = { type: "positive" | "negative"; index: number };
+
+export const calculateDivergence = (
+  prices: Array<number>,
+  rsi: Array<number>
+): Array<Divergence> => {
+  return prices.reduce((divergences: Array<Divergence>, price, index) => {
+    if (index === 0) return divergences;
+    // Positive Divergence
+    if (price < prices[index - 1] && rsi[index] > rsi[index - 1]) {
+      divergences.push({ type: "positive", index });
+    }
+    // Negative Divergence
+    else if (price > prices[index - 1] && rsi[index] < rsi[index - 1]) {
+      divergences.push({ type: "negative", index });
+    }
+    return divergences;
+  }, []);
+};
--- src/indicators/index.ts
+++ src/indicators/index.ts
@@ -1,2 +1,3 @@
 export * from "./sma";
 export * from "./rsi";
+export * from "./divergence";
